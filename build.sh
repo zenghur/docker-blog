85f6ba0ab99efff446d037fa821f23f7e26bc550
zenghur <254715579@qq.com>
2019-10-13 00:04:04 +0800

    feat: build

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
+rm -rf blog/public
+cd blog && hugo && cd ..
 cp ../key.pem ../cert.pem . && git pull origin master && docker build -t blog:dev ./ 
 docker container stop myblog 
 docker run -p 443:443 -p 80:80 --rm --name myblog -d blog:dev 
 rm key.pem cert.pem
+rm -rf blog/public
 
